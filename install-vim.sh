69e3c33ca4667afc890c44635ff75fc16dd77afb
Сиденко Кирилл Сергеевич <k.sidenko@2gis.ru>
2016-01-10 10:05:06 +0600

    add tmux plugin manager

--- install-vim.sh
+++ install-vim.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 curl https://raw.githubusercontent.com/ksidenko/spf13-vim/3.0-new/bootstrap.sh -L > spf13-vim.sh && sh spf13-vim.sh
+
+# install tmux plugin mnager
+# https://github.com/tmux-plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
