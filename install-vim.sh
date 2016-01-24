08e5fda78d595ea6d68153f18baabaf13eb96eed
Сиденко Кирилл Сергеевич <k.sidenko@2gis.ru>
2016-01-24 11:56:58 +0600

    fix prepare for install xkbswitch

--- install-vim.sh
+++ install-vim.sh
@@ -6,4 +6,10 @@ curl https://raw.githubusercontent.com/ksidenko/spf13-vim/3.0-new/bootstrap.sh -
 # https://github.com/tmux-plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Prepare for xkbswitch vim plugin
+curl https://github.com/myshov/xkbswitch-macosx/raw/master/bin/xkbswitch -L > xkbswitch
+chmod +x xkbswitch
+mv xkbswitch /usr/local/bin/
 
+curl https://github.com/myshov/libxkbswitch-macosx/raw/master/bin/libxkbswitch.dylib -L > libxkbswitch.dylib
+mv libxkbswitch.dylib /usr/local/lib/
